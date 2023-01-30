577e968a94bc7f628ae29a57ad6cf23656ec64f1
Eugene Marinovich <eugene.m@razortheory.com>
2023-01-30 18:10:29 +0300

    feat: add depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,10 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { graphql } from 'graphql';
+import { graphql, validate, parse } from 'graphql';
+import * as depthLimit from 'graphql-depth-limit';
+
 import { schema } from './components/schema';
 import { graphqlBodySchema } from './schema';
+import { DEPTH_LIMIT } from './constants';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -15,6 +18,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
     },
     async function (request, reply) {
       if (!request.body.query) throw fastify.httpErrors.badRequest();
+
+      const result = validate(schema, parse(request.body.query), [
+        depthLimit(DEPTH_LIMIT),
+      ]);
+
+      if (result.length) {
+        reply.send({ errors: result });
+      }
 
       return await graphql({
         schema: schema,
